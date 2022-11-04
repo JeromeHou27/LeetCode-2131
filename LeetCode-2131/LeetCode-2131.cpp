ffb8fe4c5f54b3bfa62580f07586acfdbd7f6bd6
jerome.hou <jerome.hou@jkos.com>
2022-11-04 11:40:07 +0800

    feat: Completed

---
+++ LeetCode-2131/LeetCode-2131.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+
+using namespace std;
+
+struct Counter {
+    int plusCount = 0;
+    int minusCount = 0;
+};
+
+int longestPalindrome(vector<string>& words) {
+    const char alphabetCount = 'z' - 'a' + 1;
+    Counter* wordsArray[alphabetCount];
+
+    for (char i = 0; i < alphabetCount; ++i) {
+        wordsArray[i] = new Counter[alphabetCount - i];
+    }
+
+    for (string& word : words) {
+        if (word[1] < word[0]) {
+            wordsArray[word[1] - 'a'][word[0] - word[1]].minusCount++;
+        }
+        else {
+            wordsArray[word[0] - 'a'][word[1] - word[0]].plusCount++;
+        }
+    }
+
+    bool centerWord = false;
+    int worldCount = 0;
+    char secOffset = 0;
+    for (char i = 0; i < alphabetCount; ++i) {
+        secOffset = alphabetCount - i;
+
+        worldCount += wordsArray[i][0].plusCount / 2;
+        if (!centerWord)
+            centerWord = wordsArray[i][0].plusCount % 2 == 1;
+
+        for (char j = 1; j < secOffset; ++j) {
+            worldCount += wordsArray[i][j].plusCount > wordsArray[i][j].minusCount ? wordsArray[i][j].minusCount : wordsArray[i][j].plusCount;
+        }
+    }
+
+    return (worldCount * 2 + (centerWord ? 1 : 0)) * 2;
+}
+
+int main()
+{
+    vector<string> words = { "tw","az","fb" };
+
+    printf("%d\n", longestPalindrome(words));
+}
+
